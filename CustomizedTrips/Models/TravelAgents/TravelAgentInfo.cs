b5b7c8349957131fe017a5f0b89c90a58d2e8814
DAVID HO <davidhorph@hotmail.com>
2018-08-15 23:10:36 -0500

    Initial changes.

---
+++ CustomizedTrips/Models/TravelAgents/TravelAgentInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomizedTrips.Models.TravelAgents
+{
+    public class TravelAgentInfo
+    {
+        [Key]
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string email { get; set; }
+        public int? phoneNumber { get; set; }
+        public string YearsExperience { get; set; }
+        public string FavoriteDestinations { get; set; }
+    }
+}
---
+++ CustomizedTrips/Models/Traveler/VacationRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomizedTrips.Models.Traveler
+{
+    public class VacationRequest
+    {
+        [Key]
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Destination { get; set; }
+        public string DestinationDescription { get; set; }
+        public double Budget { get; set; }
+        public string DestinationDetails { get; set; }
+        public int? PhoneNumber { get; set; }
+        public string email { get; set; }
+    }
+}
